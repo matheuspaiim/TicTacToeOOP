8ce99aa12f13cc0a9aff28214ff6737ed606c18a
Matheus Paim <65717016+matheuspaiim@users.noreply.github.com>
2022-07-07 13:50:05 -0300

    starting window structure

--- package/mainframe/menu.py
+++ package/mainframe/menu.py
@@ -1,32 +1,62 @@
 from tkinter import *
 from package.functionalities.difficulties import Easy, Medium, Hard, Human_Player
+import tkinter as tk
 
-class Menu():
+
+SMALL_FONT = ('Sans-serif', 10, 'bold')
+
+class Window(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        window = tk.Frame(self)
+        
+        window.pack(side="top", fill="both", expand= True)
+        
+        window.grid_rowconfigure(0, weight=1)
+        window.grid_columnconfigure(0, weight=1)
     
-    def initialize():
+        self.frames = {}
         
-        window = Tk()
-        window.title('Menu')
+        frame = Menu(window, self)
         
+        self.frames[Menu] = frame
         
-        def restart():
-            print('teste funcionamento')
+        frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(Menu)
+
+    def show_frame(self, cont):
+        
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class Menu(tk.Frame):
     
-                        
-        human_button = Button(window, text='Jogar contra um amigo', font=('Sans-serif', 10, 'bold'), padx=15, pady=2, command=Human_Player.human_play)
-        human_button.place(x=10, y=10)
+    def initialize():   
+         
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
         
-        easy_button = Button(window, text='Fácil', font=('Sans-serif', 10, 'bold'), padx=10, pady=2, command=Easy.easy_play)
-        easy_button.place(x=65, y=50)  
         
-        medium_button = Button(window, text='Médio', font=('Sans-serif', 10, 'bold'), padx=10, pady=2, command=Medium.medium_play)
-        medium_button.place(x=62, y=85)  
+            def restart():
+                print('teste funcionamento')
         
-        hard_button = Button(window, text='Difícil', font=('Sans-serif', 10, 'bold'), padx=10, pady=2, command=Hard.hard_play)
-        hard_button.place(x=63, y=120)     
+                            
+            human_button = Button(window, text='Jogar contra um amigo', font=SMALL_FONT, padx=15, pady=2, command=Human_Player.human_play)
+            human_button.place(x=10, y=10)
             
-        reset_button = Button(window, text='Restart', font=('Sans-serif', 10, 'bold'), padx=10, pady=2, command=restart)
-        reset_button.place(x=60, y=160)
+            easy_button = Button(window, text='Fácil', font=SMALL_FONT, padx=10, pady=2, command=Easy.easy_play)
+            easy_button.place(x=65, y=50)  
+            
+            medium_button = Button(window, text='Médio', font=SMALL_FONT, padx=10, pady=2, command=Medium.medium_play)
+            medium_button.place(x=62, y=85)  
+            
+            hard_button = Button(window, text='Difícil', font=SMALL_FONT, padx=10, pady=2, command=Hard.hard_play)
+            hard_button.place(x=63, y=120)     
+                
+            reset_button = Button(window, text='Restart', font=SMALL_FONT, padx=10, pady=2, command=restart)
+            reset_button.place(x=60, y=160)
         
-        window.mainloop()
+window = Window()
+window.mainloop()
         
