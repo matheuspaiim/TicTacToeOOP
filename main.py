80e4310b6642f3410c079fa3bb6d570c7e0f8d9c
Matheus Paim <65717016+matheuspaiim@users.noreply.github.com>
2022-08-26 12:38:30 -0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from package import Menu
+from tkinter import *
+from package.mainframe.menu import Window
 
 class Main():
-    ...
+    def start(self, cont):
+        Window.show_frame(self, cont)
+        
